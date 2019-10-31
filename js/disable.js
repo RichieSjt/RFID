f8059e1511c42aa19decf46c57062c2a94b6c55f
Richie_Sjt <richi252012@hotmail.com>
2019-10-30 22:39:17 -0600

    Heatmap testing

---
+++ js/disable.js
@@ -0,0 +1,17 @@
+const appendChild = Element.prototype.appendChild;
+
+const urlCatchers = [
+"/AuthenticationService.Authenticate?",
+"/QuotaService.RecordEvent?"
+];
+
+Element.prototype.appendChild = function (element) {
+const isGMapScript = element.tagName === 'SCRIPT' && /maps\.googleapis\.com/i.test(element.src);
+const isGMapAccessScript = isGMapScript && urlCatchers.some(url => element.src.includes(url));
+
+if (!isGMapAccessScript) {
+    return appendChild.call(this, element);
+  }
+
+  return element;
+};
